f7b296e2bcb33c5bff26cbefdd19c62cca77d53a
Sebastian Rodriguez <smrodriguezcamero@gmail.com>
2026-07-11 15:13:03 -0400

    push de la PWA android

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'finanzas-cache-v1';
-const ASSETS = ['./', './index.html', './styles.css', './script.js', './manifest.json'];
+const ASSETS = ['./', './index.html', './styles.css', './script.js', './manifest.json', './icons/icon.svg'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
